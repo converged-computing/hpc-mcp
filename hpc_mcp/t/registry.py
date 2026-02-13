b274dcbbec0f9a5f1bbbb631f168fb1a5352583e
vsoch <vsoch@users.noreply.github.com>
2026-02-13 06:39:21 -0800

    feat: start of work for optimize
    right now we just need the sleep / wait timer function
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- hpc_mcp/t/registry.py
+++ hpc_mcp/t/registry.py
@@ -1,6 +1,9 @@
 import hpc_mcp.t.build.docker as docker
+import hpc_mcp.t.utils as utils
 
 TOOLS = [
+    # utilities
+    utils.sleep_timer,
     # docker build tools
     docker.docker_run_container,
     docker.docker_push_container,
