645a4a7f2dd9cf3f1a90764a20229741d8fafa08
vsoch <vsoch@users.noreply.github.com>
2026-04-20 22:01:26 -0700

    feat: support for filesystem discovery
    we are providing a controller (quasi sandbox) to read and write.
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- hpc_mcp/utils/text.py
+++ hpc_mcp/utils/text.py
@@ -1,8 +1,35 @@
+import os
 import re
 
 
 def format_rules(rules):
     return "\n".join([f"- {r}" for r in rules])
+
+
+def envar_get_boolean(name):
+    """
+    Sniff the environment for a boolean value
+    """
+    value = os.environ.get(name)
+    if not value:
+        return False
+    if value is not None and value.lower() in ["true", "t", "y", "yes", "1"]:
+        return True
+    if value is not None and value.lower() in ["false", "f", "n", "no", "0"]:
+        return False
+    return False
+
+
+def envar_get_integer(name):
+    """
+    Sniff the environment for an integer value
+    """
+    value = os.environ.get(name)
+    if value is None:
+        return value
+    if value.isdigit():
+        return int(value)
+    return None
 
 
 def get_code_block(content, code_type):
